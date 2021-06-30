b253a0423376cd2bf50b53db4680849bf07d0994
hernan caballero <hernan_1587@hotmail.com>
2021-06-30 17:23:15 -0300

    intento de arreglo del mail

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var nodemailer= require('nodemailer');
+var nodemailer = require('nodemailer');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index');
 });
 router.post('/', async (req, res, next) => {
@@ -12,6 +12,26 @@ router.post('/', async (req, res, next) => {
   var tel = req.body.tel;
   var mensaje = req.body.comentarios;
   console.log(req.body);
-})
+
+  var obj = {
+    to: 'hernan_1587@hotmail.com',
+    subject: 'comentacto desde la web',
+    html: nombre + " " + apellido + " se contacto atraves y quiere mas info a este correo: " + email + ", <br> Ademas, hizo el siguiente comentario: " + telefono
+  }//cierra var obj
+  var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "df147c916b10df",
+      pass: "e4f154bfe21119"
+    }
+  });// cierra transporter
+   var info = await transport.sendMail(obj);
+
+   res.render('index', {
+     message: 'mensaje enviado correctamente',
+   })
+
+})// cierra peticion del post
 
 module.exports = router;
