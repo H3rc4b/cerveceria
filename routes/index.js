46d5ed22aa5603484f78236da52337bcdc31a38a
hernan caballero <hernan_1587@hotmail.com>
2021-06-30 14:58:37 -0300

    re configurado

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var nodemailer= require('nodemailer');
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+router.post('/', async (req, res, next) => {
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var tel = req.body.tel;
+  var mensaje = req.body.comentarios;
+  console.log(req.body);
+})
 
 module.exports = router;
