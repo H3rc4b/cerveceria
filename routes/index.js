435acd4195ef1c01ffe47bfbee18267fdb9c4a8f
hernan caballero <hernan_1587@hotmail.com>
2021-08-03 22:33:36 -0300

    correcion envio de mail

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index');
 });
+
 router.post('/', async (req, res, next) => {
   var nombre = req.body.nombre;
   var apellido = req.body.apellido;
   var email = req.body.email;
   var tel = req.body.tel;
- // var mensaje = req.body.mensaje;
+  var mensaje = req.body.comentarios;
   console.log(req.body);
 
   var obj = {
     to: 'hernan_1587@hotmail.com',
     subject: 'comentacto desde la web',
-    html: nombre + " " + apellido + " se contacto atraves y quiere mas info a este correo: " + email + ", <br> Ademas, hizo el siguiente comentario: " + tel
+    html: nombre + " " + apellido + " se contacto y quiere mas info a este correo: " + email + ", <br> Ademas, hizo el siguiente comentario: " + mensaje + ", este es su telefno: " + tel
   }//cierra var obj
-  var transport = nodemailer.createTransport({
+
+  var transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     auth: {
@@ -26,10 +29,10 @@ router.post('/', async (req, res, next) => {
       pass: process.env.SMTP_PASS
     }
   });// cierra transporter
-   var info = await transport.sendMail(obj);
+   var info = await transporter.sendMail(obj);
    res.render('index', {
      message: 'mensaje enviado correctamente',
-   })
+   });
 
 })// cierra peticion del post
 
