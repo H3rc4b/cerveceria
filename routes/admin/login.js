74620ed5d424af26063ac1dde7f12d5db875c4f5
hernan caballero <hernan_1587@hotmail.com>
2021-08-05 20:25:20 -0300

    error

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var usuariosmodel = require('./../../models/usuariosmodel');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('admin/login',{
-      layout:'admin/layout' 
-  } );
+  res.render('admin/login', {
+    layout: 'admin/layout'
+  });
 });
+
+router.post('/', async (req, res, next) => {
+  try {
+    var usuario = req.body.usuario;
+    var password = req.body.password;
+
+    console.log(req.body);
+
+    var data = await usuariosmodel.getUserandPassword(usuario, password);
+    if (data != undefined) {
+      res.redirect('/admin/novedades');
+    } else {
+      res.render('admin/login', {
+        layout: 'admin/layout',
+        error: true
+      })
+    }// cierre else
+  } catch(error) {
+    console.log(error)
+  }
+});
+
+
 module.exports = router;
